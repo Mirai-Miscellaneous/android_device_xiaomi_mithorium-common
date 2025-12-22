0a9cb5a9ab48b1e8d2c30e277cfc717644d6da40
mizukanaAlt <rina301087@gmail.com>
2025-12-22 13:41:21 +0000

    mithorium-common: sepolicy: remove qti-thermal HAL
    not working properly on msm8953 K4.19

--- mithorium.mk
+++ mithorium.mk
@@ -545,12 +545,6 @@ PRODUCT_BOOT_JARS += \
     telephony-ext
 endif
 
-# Thermal
-ifneq ($(TARGET_KERNEL_VERSION),4.19)
-MITHORIUM_PRODUCT_PACKAGES += \
-    android.hardware.thermal@2.0-service.qti.xiaomi_mithorium
-endif
-
 # Trust HAL
 MITHORIUM_PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
